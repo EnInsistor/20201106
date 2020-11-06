409f9a58c61a53a867ec9aaf5d8216f6d667901a
EnInsistor <73988969+EnInsistor@users.noreply.github.com>
2020-11-06 08:59:34 +0800

    Update User.java
    on githug update

--- src/main/java/cc/insistor/ssh/User.java
+++ src/main/java/cc/insistor/ssh/User.java
@@ -1,7 +1,11 @@
 package cc.insistor.ssh;
 
 public class User {
-    public void main(){
+    public void main1(){
         System.out.println("cccc");
     }
+     public void main2(){
+        System.out.println("cccc");
+    }
+    
 }
